694972c5ef0ddd143ada53d2b39fca5b31387e9f
Yifan Chen <Yifan@staff-200-109.eduroam.unibe.ch>
2017-12-08 15:50:59 +0100

    V4

--- tests/test_utilities.cpp
+++ tests/test_utilities.cpp
@@ -229,7 +229,7 @@ TEST(Interpolation, BaryCentric) {
     DisplVector[3] = {10.0, 11.0, 12.0};
 
 
-    ThreeVector<float> Location = {0., 1., 0.};
+    ThreeVector<float> Location = {0., 0., 1.};
 
 //    ASSERT_TRUE(Location == ThreeVector<float>(0.5, 0.5, 0.5));
 
@@ -346,9 +346,9 @@ std::vector<float> BaryCoord;
 //    ASSERT_NEAR(InterpolatedDispl[1],1.,1E-3);
 //    ASSERT_NEAR(InterpolatedDispl[2],1.,1E-3);
 
-    ASSERT_EQ(InterpolatedDispl[0],7.);
-    ASSERT_EQ(InterpolatedDispl[1],8.);
-    ASSERT_EQ(InterpolatedDispl[2],9.);
+    ASSERT_EQ(InterpolatedDispl[0],10.);
+    ASSERT_EQ(InterpolatedDispl[1],11.);
+    ASSERT_EQ(InterpolatedDispl[2],12.);
 
 
 
